4a1522a588629c8e2f3b1f89b9f9aa167b967e7d
timo-piirmann <timurlane5@gmail.com>
2017-11-09 10:38:56 +0200

    Change apikey url [#152191241]

--- src/main/java/au/org/ala/biocache/web/AbstractSecureController.java
+++ src/main/java/au/org/ala/biocache/web/AbstractSecureController.java
@@ -1,12 +1,12 @@
 /**************************************************************************
  *  Copyright (C) 2013 Atlas of Living Australia
  *  All Rights Reserved.
- * 
+ *
  *  The contents of this file are subject to the Mozilla Public
  *  License Version 1.1 (the "License"); you may not use this file
  *  except in compliance with the License. You may obtain a copy of
  *  the License at http://www.mozilla.org/MPL/
- * 
+ *
  *  Software distributed under the License is distributed on an "AS
  *  IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
  *  implied. See the License for the specific language governing
@@ -35,7 +35,7 @@ public class AbstractSecureController {
 
     private final static Logger logger = LoggerFactory.getLogger(AbstractSecureController.class);
 
-    @Value("${api.check.url:https://auth.ala.org.au/apikey/ws/check?apikey=}")
+    @Value("${api.check.url:https://demo.elurikkus.ut.ee/apikey/ws/check?apikey=}")
     protected String apiCheckUrl;
 
     @Value("${api.check.enabled:true}")
@@ -43,8 +43,8 @@ public class AbstractSecureController {
 
     /** Local cache of keys */
     private static Set<String> apiKeys = new HashSet<String>();
-    
-    public AbstractSecureController(){}
+
+    public AbstractSecureController() {}
 
     /**
      * Check that the request has the necessary permissions to perform this request.
@@ -66,49 +66,49 @@ public class AbstractSecureController {
      * @return
      * @throws Exception
      */
-    public boolean shouldPerformOperation(String apiKey, HttpServletResponse response) throws Exception{
+    public boolean shouldPerformOperation(String apiKey, HttpServletResponse response) throws Exception {
         return shouldPerformOperation(apiKey, response, true);
     }
-    
+
     /**
      * Use a webservice to validate a key
-     * 
+     *
      * @param keyToTest
      * @return
      */
-    public boolean isValidKey(String keyToTest){
+    public boolean isValidKey(String keyToTest) {
 
-        if(!apiKeyCheckedEnabled){
+        if(!apiKeyCheckedEnabled) {
             return true;
         }
 
-        if(StringUtils.isBlank(keyToTest)){
+        if(StringUtils.isBlank(keyToTest)) {
             return false;
         }
 
-    	if(!apiKeys.contains(keyToTest)){
-    		//check via a web service
-    		try {
-    			logger.debug("Checking api key: " + keyToTest);
-	    		String url = apiCheckUrl + keyToTest;
-	    		ObjectMapper om = new ObjectMapper();
-	    		Map<String,Object> response = om.readValue(new URL(url), Map.class);
-	    		logger.debug("Checking api key: " + keyToTest + ", valid: " + response.get("valid"));
-	    		boolean isValid = (Boolean) response.get("valid");
-	    		if(isValid){
-	    			apiKeys.add(keyToTest);
-	    		}
-	    		return isValid; 
-    		} catch (Exception e){
-    			logger.error(e.getMessage(), e);
-    		}
-    	} else {
-    		return true;
-    	}
-    	return false;
+        if(!apiKeys.contains(keyToTest)) {
+            //check via a web service
+            try {
+                logger.debug("Checking api key: " + keyToTest);
+                String url = apiCheckUrl + keyToTest;
+                ObjectMapper om = new ObjectMapper();
+                Map<String,Object> response = om.readValue(new URL(url), Map.class);
+                logger.debug("Checking api key: " + keyToTest + ", valid: " + response.get("valid"));
+                boolean isValid = (Boolean) response.get("valid");
+                if(isValid) {
+                    apiKeys.add(keyToTest);
+                }
+                return isValid;
+            } catch (Exception e) {
+                logger.error(e.getMessage(), e);
+            }
+        } else {
+            return true;
+        }
+        return false;
     }
 
-	/**
+    /**
      * Returns true when the operation should be performed.
      *
      * @param apiKey
@@ -116,10 +116,10 @@ public class AbstractSecureController {
      * @return
      * @throws Exception
      */
-    public boolean shouldPerformOperation(String apiKey,HttpServletResponse response, boolean checkReadOnly)throws Exception{
-        if(checkReadOnly && Store.isReadOnly()){
-            response.sendError(HttpServletResponse.SC_CONFLICT, "Server is in read only mode.  Try again later.");
-        } else if(!isValidKey(apiKey)){
+    public boolean shouldPerformOperation(String apiKey, HttpServletResponse response, boolean checkReadOnly) throws Exception {
+        if(checkReadOnly && Store.isReadOnly()) {
+            response.sendError(HttpServletResponse.SC_CONFLICT, "Server is in read only mode. Try again later.");
+        } else if(!isValidKey(apiKey)) {
             response.sendError(HttpServletResponse.SC_FORBIDDEN, "An invalid API Key was provided.");
         }
         return !response.isCommitted();
